d2a3851119e4df7f333fb5418b6cfa8c5e7991ef
samyakjain96 <56966680+samyakjain96@users.noreply.github.com>
2021-06-15 16:56:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-@app.route("/ank")
-def hello():
-    return "Hello, Ankit!"
+@app.route('/user/<name>')
+def user(name):
+    personal = f'<h1>Hello, {name}!</h1>'
+    # above - the curly braces {} hold a variable; when this runs,
+    # the value will replace the braces and the variable name
+    instruc = '<p>Change the name in the <em>browser address bar</em> \
+        and reload the page.</p>'
+    return personal + instruc
